2f2c4f3679c99d0b44de3c8c3690c8f9babfa5d0
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-10-24 19:14:58 +0700

    React Bootstrap #7 - Use grid, pagination and map function to work on our works

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import AppHeader from "./components/header";
 import AppHero from "./components/hero";
 import AppAbout from "./components/abou";
 import AppServices from "./components/services";
+import AppWorks from "./components/works";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <AppHero />
         <AppAbout />
         <AppServices />
+        <AppWorks />
       </main>
     </div>
   );
